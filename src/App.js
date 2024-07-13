ae2fc8dafde2ba054d39dedc8bd4c5814a07d7c0
Rahul Singh <rahul.singh@touchstonellp.com>
2024-07-13 12:24:06 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Layout from './component/Layout/layout';
 
 function App() {
   return (
     <>
+    <Analytics/>
+    <SpeedInsights/>
     <Layout/>
     </>
   );
